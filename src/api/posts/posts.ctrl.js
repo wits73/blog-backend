af20512d53ee5320d9d2bbe95dc560f1008354c7
Tae Kim <tkim25@kennesaw.edu>
2018-09-24 16:54:26 -0400

    added koa-router

---
+++ src/api/posts/posts.ctrl.js
@@ -0,0 +1,105 @@
+let postId = 1;
+
+const posts = [
+    {
+        id: 1,
+        title: 'title',
+        body: 'contents'
+    }
+];
+
+exports.write = (ctx) => {
+    const{
+        title,
+        body
+    } = ctx.request.body;
+
+    postId += 1;
+
+    const post = {id: postId, title, body };
+    posts.push(post);
+    ctx.body = post;
+}
+
+exports.list = (ctx) => {
+    ctx.body = posts;
+}
+
+exports.read = (ctx) => {
+    const{
+        id
+    } = ctx.params;
+
+    const post = posts.find(p => p.id.toString() === id);
+
+    if(!post) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'Post not exsits'
+        };
+        return;
+    }
+    ctx.body = post;
+}
+
+exports.remove = (ctx) => {
+    const{
+        id
+    } = ctx.params;
+
+    const index = posts.findIndex(p => p.id.toString() === id);
+
+    if(index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'Post not exsits'
+        };
+        return;
+    }
+    posts.splice(index,1)
+    ctx.status = 204;
+}
+
+exports.replace = (ctx) => {
+    const{
+        id
+    } = ctx.params;
+
+    const index = posts.findIndex(p => p.id.toString() === id);
+
+    if(index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'Post not exsits'
+        };
+        return;
+    }
+
+    posts[index] = {
+        id,
+        ...ctx.request.body
+    };
+    ctx.body = posts[index];
+}
+
+exports.update = (ctx) => {
+    const{
+        id
+    } = ctx.params;
+
+    const index = posts.findIndex(p => p.id.toString() === id);
+
+    if(index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'Post not exsits'
+        };
+        return;
+    }
+
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body
+    };
+    ctx.body = posts[index];
+}
